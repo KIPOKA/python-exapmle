db88de48db4588a7ea9fa8f9f29075307011cd7d
Katabe <ladykipoka@gmail.com>
2022-09-13 13:01:16 +0200

    Fixed

--- my_site/my_office/views.py
+++ my_site/my_office/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def list_patient(request):
+    all_patients = models.Patient.objects.all()
+    context = {'patients': all_patients}
+    return render(request, 'my_office/list.html',context)
